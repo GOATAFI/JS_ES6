e1b8cffd213a7d958944d3b3401e11def726e293
GOATAFI <projectexecutiveofpm@gmail.com>
2024-11-08 22:47:58 +0600

    let var const

---
+++ index.js
@@ -0,0 +1,27 @@
+const a = 10;
+const b = 20;
+const sum = (a, b) => a + b;
+
+console.log(sum(a, b));
+
+const arr = [];
+
+arr.push(10);
+arr.push(14);
+
+arr.push(20);
+
+var c = 20;
+if (true) {
+  var c = 30;
+  console.log(c);
+}
+
+console.log(c);
+
+let d = 40;
+if (true) {
+  let d = 5;
+  console.log(d);
+}
+console.log(d);
