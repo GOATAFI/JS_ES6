4ce5359c4068eac9e05d5d50ca23d5bf6046f011
GOATAFI <projectexecutiveofpm@gmail.com>
2024-11-09 23:55:25 +0600

    arrow func

---
+++ arrow.js
@@ -0,0 +1,5 @@
+const print = () => {
+  console.log("Hello arrow function");
+};
+
+print();
---
+++ templateString.js
@@ -0,0 +1,12 @@
+const name = "Nahid Parvez Mafi";
+const age = 24;
+const email = "nahid@gmail.com";
+
+const details = "Name: " + name + "\n Age: " + age + " Email:" + email;
+
+const details2 = `Name: ${name} 
+Age: ${age} 
+Email:${email}`;
+
+console.log(details);
+console.log(details2);
